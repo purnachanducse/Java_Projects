6ba3d85ff8e3abb73d98e1d32b98bfbabe990fe9
purnachanducse <purnachandu.sapparapu@lntinfotech.com>
2019-10-29 16:03:00 +0530

    Adding JUnit

---
+++ JUnit/src/test/java/com/test/junit/MyMathTest.java
@@ -0,0 +1,40 @@
+package com.test.junit;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MyMathTest {
+	
+	@Test
+	public void sum_with3numbers() {
+		MyMath myMath = new MyMath();
+		int sum = myMath.sum(new int[] {1,2,3});
+		assertEquals(6, sum);
+		//fail("not yet implemented");
+	}
+	
+	@Before
+	public void before() {
+		System.out.println("Before");
+	}
+	
+	@After
+	public void after() {
+		System.out.println("After");
+	}
+	
+	@BeforeClass
+	public static void beforeClass() {
+		System.out.println("Before class");
+	}
+	
+	@AfterClass
+	public static void afterClass() {
+		System.out.println("After class");
+	}
+}
